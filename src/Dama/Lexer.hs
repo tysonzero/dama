52e13a51d381c6689f295791a7914638e1a4aba7
tysonzero <danielspencersmith@gmail.com>
2017-02-16 14:11:05 -0500

    Use Error instead of String in Lexer

--- src/Dama/Lexer.hs
+++ src/Dama/Lexer.hs
@@ -6,10 +6,11 @@ import Data.Char (isAlpha, isLower, isUpper)
 import Data.Monoid ((<>))
 
 import Dama.CharX
+import Dama.Error
 import Dama.Location
 import Dama.Token
 
-type Lexer = [CharX] -> Either String [Token]
+type Lexer = [CharX] -> Either Error [Token]
 
 lex :: Lexer
 lex ((l, c) : cs)
@@ -19,14 +20,14 @@ lex ((l, c) : cs)
     | isFreeSymbol c = lexIdentifier l IdSymbol (c :) cs
     | isLower c = lexIdentifier l IdLower (c :) cs
     | isUpper c = lexIdentifier l IdUpper (c :) cs
-    | otherwise = Left $ unexpected l c
+    | otherwise = Left (l, "Unexpected character " <> show c)
 lex [] = Right []
 
 lexIdentifier :: Location -> (String -> Tok) -> (String -> String) -> Lexer
 lexIdentifier s t a ((l, c) : cs)
     | c `elem` " \n" = ((s, catchReserved . t $ a []) :) <$> lex cs
     | isAlpha c || isFreeSymbol c = lexIdentifier s t (a . (c :)) cs
-    | otherwise = Left $ unexpected l c
+    | otherwise = Left (l, "Unexpected character " <> show c)
 lexIdentifier s t a [] = Right [(s, catchReserved . t $ a [])]
 
 catchReserved :: Tok -> Tok
@@ -35,6 +36,3 @@ catchReserved t = t
 
 isFreeSymbol :: Char -> Bool
 isFreeSymbol = (`elem` "!$%&*+-./<=>^")
-
-unexpected :: Location -> Char -> String
-unexpected l c = "Unexpected character " <> show c <> " at " <> render l
