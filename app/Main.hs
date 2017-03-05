c4ea6a2c20a637510d727ed882fe406a524c9f6d
tysonzero <danielspencersmith@gmail.com>
2017-03-05 03:01:11 -0500

    Actually implement the executable itself

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,25 @@
 module Main (main) where
 
+import Prelude hiding (lex)
+
+import Data.Monoid ((<>))
+import System.Environment (getArgs)
+
+import Dama.Annotator (annotate)
+import Dama.Lexer (lex)
+import Dama.Parser (parse)
+
 main :: IO ()
-main = putStrLn "Dama not yet implemented"
+main = getArgs >>= \case
+    mode : filename : _ -> do
+        program <- readFile filename
+        let annotated = annotate filename program
+            lexed = lex annotated
+            parsed = parse =<< lexed
+        case mode of
+            "annotate" -> print annotated
+            "lex" -> print lexed
+            "parse" -> print parsed
+            _ -> putStrLn $ "invalid mode: " <> mode
+    [_] -> putStrLn "no input file"
+    [] -> putStrLn "no mode specified"
