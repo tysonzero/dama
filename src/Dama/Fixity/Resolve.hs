cb25017dc9f8900179b667479b4c04b83e837db1
tysonzero <danielspencersmith@gmail.com>
2017-04-07 02:05:41 -0400

    Create Dama.Fixity.Resolve

---
+++ src/Dama/Fixity/Resolve.hs
@@ -0,0 +1,7 @@
+module Dama.Fixity.Resolve (resolveFixity) where
+
+import qualified Dama.Parser.AST as AST
+import qualified Dama.Fixity.IR as IR
+
+resolveFixity :: AST.Program -> IR.Program
+resolveFixity = error "resolveFixity"
