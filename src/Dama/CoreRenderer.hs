2319cd10fdad3df6c64b124be5c2610b3a95544a
tysonzero <danielspencersmith@gmail.com>
2017-04-06 20:18:04 -0400

    Create CoreRenderer for pretty one-line printing

---
+++ src/Dama/CoreRenderer.hs
@@ -0,0 +1,31 @@
+module Dama.CoreRenderer (renderCore) where
+
+import Data.List (intercalate)
+import Data.Maybe (fromMaybe)
+import Data.Monoid ((<>))
+import Dama.Core
+
+renderCore :: Program -> String
+renderCore ds = unwords $ renderDec <$> ds
+  where
+    renderDec (n := e) = n <> " = " <> renderExpr e
+
+renderExpr :: Expr -> String
+renderExpr (Var s) = s
+renderExpr (App f x) = renderExpr f <> " " <> renderExpr x
+renderExpr (Lam b e) = "\\ " <> renderBind b <> " -> " <> renderExpr e
+renderExpr (Case e os) = renderExpr e <> " => { " <> intercalate " | " (renderOption <$> os) <> " }"
+renderExpr (Let ds e) = "{ " <> intercalate " | " (renderDecl <$> ds) <> " } => " <> renderExpr e
+
+renderDecl :: Decl -> String
+renderDecl (n := e) = n <> " = " <> renderExpr e
+
+renderOption :: Option -> String
+renderOption (p :-> e) = renderPattern p <> " -> " <> renderExpr e
+
+renderPattern :: Pattern -> String
+renderPattern (Bind b) = renderBind b
+renderPattern (Cons c bs) = c <> ((' ' :) . renderBind =<< bs)
+
+renderBind :: Bind -> String
+renderBind = fromMaybe "_" 
