6afe7ffaccfff64af14e85c883418e4ee3667c95
tysonzero <danielspencersmith@gmail.com>
2017-04-18 04:18:01 -0400

    Add Lam and Show to Fixity IR

--- src/Dama/Fixity/IR.hs
+++ src/Dama/Fixity/IR.hs
@@ -2,15 +2,21 @@ module Dama.Fixity.IR
     ( Program
     , Decl(Decl)
     , Pattern(Bind, Cons)
-    , Expr(App)
+    , Expr(Lit, App, Lam)
     ) where
 
 type Program = [Decl]
 
 data Decl = Decl String [Pattern] Expr
+    deriving Show
 
 data Pattern
     = Bind String
     | Cons String [Pattern]
+    deriving Show
 
-data Expr = App String [Expr]
+data Expr
+    = Lit String
+    | App Expr Expr
+    | Lam String Expr
+    deriving Show
